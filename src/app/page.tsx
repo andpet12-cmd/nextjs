4f970fc3c9c796b3e6d015b6ea109bba4313ba4e
andpet12-cmd <andrpetr18912@gmail.com>
2025-02-07 17:59:20 +0200

    Start HW2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,6 @@ export default function Home() {
   return (
     <div className={styles.page}>
 
-      <form action="/submit" method="POST">
-        <input type="text" name={'name'} placeholder={'login'} />
-        <input type="text" name={'password'} placeholder={'password'} />
-        <button>submit</button>
-
-      </form>
-
-
     </div>
   );
 }
